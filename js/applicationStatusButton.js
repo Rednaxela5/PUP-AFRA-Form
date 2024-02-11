c9b945ad08b7c5d35e6b63cf9bdabd3fd6ed95be
Jaira Mijares <92448135+jai-cm@users.noreply.github.com>
2024-02-12 00:20:21 +0800

    Added Application Status Button JavaScript

---
+++ js/applicationStatusButton.js
@@ -0,0 +1,13 @@
+const statusButton = document.getElementById('view-statusBTN');
+const statusTextHeader = document.getElementById('applicationStatusHeader');
+
+
+// Add event listener for back button
+statusButton.addEventListener('click', function() {
+    window.location.href = 'applicationStatus.html';
+});
+
+// Add event listener for back button
+statusTextHeader.addEventListener('click', function() {
+    window.location.href = 'applicationStatus.html';
+});
